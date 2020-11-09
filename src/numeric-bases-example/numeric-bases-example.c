c38c17b70121b4d2fe38e58b044dc97a7aa0bf69
Deiondre Andrews <andrews.deiondre@gmail.com>
2020-11-09 11:28:12 -0500

    Update with numeric bases (decimal, hexadecimal, octal) example

---
+++ src/numeric-bases-example/numeric-bases-example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = 15;
+
+    printf("Decimal: %d\n", number);
+    printf("Hexadecimal: 0x%X\n", number);
+    printf("Octal: %o\n", number);
+
+    return 0;
+}
